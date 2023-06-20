8347f62e4f9a921cb89716c5c3ee6beac8e2ee60
lecznar <130831591+lecznar@users.noreply.github.com>
2023-06-20 20:26:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,67 @@
 console.log('WARSZTAT - Walidacja formularza zapisu do Newsletter`a');
+
+console.log('WARSZTAT - Walidacja formularza zapisu do Newsletter`a');
+
+let newsletterForm = document.getElementById('newsletter-form');
+let allAgreeChx = document.getElementById('all-agree');
+
+const validate = (event) => {
+
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    if (txtName.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Imie i nazwisko!';
+        errors.appendChild(liError);
+        console.log(liError);
+    }
+
+    if (txtEmail.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz adress e-mail!';
+        errors.appendChild(liError);
+        console.log(liError);
+    }
+
+    if (!txtEmail.value.includes('@')) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Adress e-mail! musi zawierać @';
+        errors.appendChild(liError);
+        console.log(liError);
+    }
+
+    if (!agree1.checked) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Nie wyraziłeś Zgody 1!';
+        errors.appendChild(liError);
+        console.log(liError);
+    }
+
+    console.log(errors.children.length);
+
+    if (errors.children.length > 0) {
+        event.preventDefault();
+    }
+    
+    
+}
+
+const allAgree = (event) => {
+    let agree1 = document.getElementById('agree-1');
+    let agree2 = document.getElementById('agree-2');
+
+    agree1.checked = event.target.checked;
+    agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
+
+}
+
+newsletterForm.addEventListener('submit', validate);
+allAgreeChx.addEventListener('change', allAgree);
